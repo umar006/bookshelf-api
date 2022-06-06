28ef2deef3e0002fa4a4b380b374f0188801a305
umar006 <umaralfaruq597@gmail.com>
2022-06-06 13:43:42 +0700

    Add default value to created and updated coloumns

--- db/migration/000001_create_book_table.up.sql
+++ db/migration/000001_create_book_table.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS "book" (
   "id" varchar PRIMARY KEY,
-  "name" varchar,
-  "year" int,
-  "author" varchar,
+  "name" varchar NOT NULL,
+  "year" int NOT NULL,
+  "author" varchar NOT NULL,
   "summary" varchar,
   "publisher" varchar,
   "page_count" int,
   "read_page" int,
   "finished" bool,
   "reading" bool,
-  "created_at" varchar,
-  "updated_at" varchar
+  "created_at" varchar DEFAULT (extract(epoch from now()) * 1000),
+  "updated_at" varchar DEFAULT (extract(epoch from now()) * 1000)
 );
