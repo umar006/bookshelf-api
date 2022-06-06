e38cd094542bee45dab6562155df021ff3f410d0
umar006 <umaralfaruq597@gmail.com>
2022-06-06 21:21:29 +0700

    Parse book body request into json

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,21 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
+
+type Book struct {
+	Id        string `json:"bookId"`
+	Name      string `json:"name"`
+	Year      int16  `json:"year"`
+	Author    string `json:"author"`
+	Summary   string `json:"summary"`
+	Publisher string `json:"pulisher"`
+	PageCount int16  `json:"pageCount" db:"page_count"`
+	ReadPage  int16  `json:"readPage" db:"read_page"`
+	Finished  bool   `json:"finished"`
+	Reading   bool   `json:"reading"`
+	CreatedAt string `json:"insertedAt"`
+	UpdatedAt string `json:"updatedAt"`
+}
 
 var db *sqlx.DB
 
@@ -38,4 +54,11 @@ func main() {
 func InsertBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusCreated)
+
+	var book Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
