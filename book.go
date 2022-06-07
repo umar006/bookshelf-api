b631d2ebf74d0ba42d4cb87f1eb7c6dfe890a20f
umar006 <umaralfaruq597@gmail.com>
2022-06-07 12:27:59 +0700

    Fix typo and Add name tag db
    - Fix typo publisher
    - Add name tag db to createdAt and updatedAt

--- book.go
+++ book.go
@@ -6,11 +6,11 @@ type Book struct {
 	Year      int16  `json:"year"`
 	Author    string `json:"author"`
 	Summary   string `json:"summary"`
-	Publisher string `json:"pulisher"`
+	Publisher string `json:"publisher"`
 	PageCount int16  `json:"pageCount" db:"page_count"`
 	ReadPage  int16  `json:"readPage" db:"read_page"`
 	Finished  bool   `json:"finished"`
 	Reading   bool   `json:"reading"`
-	CreatedAt string `json:"insertedAt"`
-	UpdatedAt string `json:"updatedAt"`
+	CreatedAt string `json:"insertedAt" db:"created_at"`
+	UpdatedAt string `json:"updatedAt" db:"updated_at"`
 }
